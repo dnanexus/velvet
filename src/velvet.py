907dbefb063b6ac5d80566f2d1f6bae525068aaf
Andrey Kislyuk <kislyuk@gmail.com>
2013-06-15 14:16:18 -0700

    Automatic merging of split paired files

--- src/velvet.py
+++ src/velvet.py
@@ -11,6 +11,25 @@ def run_shell(command):
 def main(hash_length, **kwargs):
     velveth_cmd = "velveth velvet_wd {hash_length} -fmtAuto".format(hash_length=hash_length)
 
+    if kwargs['mergeShortPaired']:
+        dxpy.download_dxfile(kwargs['short']["$dnanexus_link"], 'short')
+        dxpy.download_dxfile(kwargs['shortPaired']["$dnanexus_link"], 'shortPaired')
+        run_shell("shuffleSeqs-fast short shortPaired shortPairedMerged")
+        del kwargs['short'], kwargs['shortPaired']
+        velveth_cmd += ' -shortPaired shortPairedMerged'
+    if kwargs['mergeShortPaired2']:
+        dxpy.download_dxfile(kwargs['short2']["$dnanexus_link"], 'short2')
+        dxpy.download_dxfile(kwargs['shortPaired2']["$dnanexus_link"], 'shortPaired2')
+        run_shell("shuffleSeqs-fast short2 shortPaired2 shortPairedMerged2")
+        del kwargs['short2'], kwargs['shortPaired2']
+        velveth_cmd += ' -shortPaired2 shortPairedMerged2'
+    if kwargs['mergeLongPaired']:
+        dxpy.download_dxfile(kwargs['long']["$dnanexus_link"], 'long')
+        dxpy.download_dxfile(kwargs['longPaired']["$dnanexus_link"], 'longPaired')
+        run_shell("shuffleSeqs-fast long longPaired longPairedMerged")
+        del kwargs['long'], kwargs['longPaired']
+        velveth_cmd += ' -longPaired longPairedMerged'
+
     for input in 'short', 'shortPaired', 'short2', 'shortPaired2', 'long', 'longPaired':
         if input in kwargs:
             dxpy.download_dxfile(kwargs[input]["$dnanexus_link"], input)
